20731d10e61873fb3f61a5e2638c576b18076881
cvltmaheshp <66312615+cvltmaheshp@users.noreply.github.com>
2022-11-02 23:04:57 -0400

    go release update

--- vendor/github.com/hashicorp/terraform-exec/tfexec/internal/e2etest/testdata/unformatted/file2.tf
+++
@@ -1,4 +0,0 @@
-resource     "foo"      "baz" {
-     baz = 1
-       qux      =        2
-}
--- vendor/github.com/hashicorp/terraform-exec/tfexec/internal/e2etest/testdata/var/main.tf
+++
@@ -1,9 +0,0 @@
-variable "default" {
-  default = "foo"
-}
-
-variable "no_default" {
-}
-
-variable "no_default_really_long_variable_name_that_will_line_wrap_tf_output" {
-}
